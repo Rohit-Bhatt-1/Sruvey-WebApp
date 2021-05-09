a26ee50f13ac66b821e6513f779f119839d2ea43
Rohit Bhatt <rohitbhatt2000@gmail.com>
2021-05-10 00:24:30 +0530

    oauth flow done

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,4 @@ module.exports =
   googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
   mongoURL : process.env.MONGO_URL,
   cookieKey : process.env.COOKIE_KEY
-  //absoluteURI : 'http:'/'/powerful-river-63987.herokuapp.com';
 };
