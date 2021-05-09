45c3149ae792cbde17966cbe66028e4efb26278e
Rohit Bhatt <rohitbhatt2000@gmail.com>
2021-05-09 19:09:36 +0530

    removed error

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,6 @@ module.exports =
 {
   googleClientID : process.env.GOOGLE_CLIENT_ID,
   googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
-  mongoURL : process.enc.MONGO_URL,
+  mongoURL : process.env.MONGO_URL,
   cookieKey : process.env.COOKIE_KEY
 };
