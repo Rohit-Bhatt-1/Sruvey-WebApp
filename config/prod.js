0da5f2ba9a98cf4187e4ef84df0c885f9d6bc69b
Rohit Bhatt <rohitbhatt2000@gmail.com>
2021-05-09 22:03:12 +0530

    debugging

--- config/prod.js
+++ config/prod.js
@@ -6,4 +6,5 @@ module.exports =
   googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
   mongoURL : process.env.MONGO_URL,
   cookieKey : process.env.COOKIE_KEY
+  absolute : 'http:'/'/powerful-river-63987.herokuapp.com';
 };
